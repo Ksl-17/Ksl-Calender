bcf23856051e44780c45f7130d71ca2ea42d59f2
Ksl-17 <105416746+Ksl-17@users.noreply.github.com>
2022-06-03 17:55:50 +0600

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,82 @@
+print('                         LAGNA Sarma                     ')
+#My First Calendar
+
+print('')
+
+print('              🥰🥰created by Lagna with love 🥰🥰         ')
+
+print('')
+import calendar
+year=2022
+month=1
+x=calendar.month(year,month)
+print(x)
+
+import calendar
+year=2022
+month=2
+x=calendar.month(year,month)
+print(x)
+
+import calendar
+year=2022
+month=3
+x=calendar.month(year,month)
+print(x)
+
+import calendar
+year=2022
+month=4
+x=calendar.month(year,month)
+print(x)
+
+import calendar
+year=2022
+month=5
+x=calendar.month(year,month)
+print(x)
+
+import calendar
+year=2022
+month=6
+x=calendar.month(year,month)
+print(x)
+
+import calendar
+year=2022
+month=7
+x=calendar.month(year,month)
+print(x)
+
+import calendar
+year=2022
+month=8
+x=calendar.month(year,month)
+print(x)
+
+import calendar
+year=2022
+month=9
+x=calendar.month(year,month)
+print(x)
+
+import calendar
+year=2022
+month=10
+x=calendar.month(year,month)
+print(x)
+
+import calendar
+year=2022
+month=11
+x=calendar.month(year,month)
+print(x)
+
+import calendar
+year=2022
+month=12
+x=calendar.month(year,month)
+print(x)
+
+
+print('Thanks for using my tool')
